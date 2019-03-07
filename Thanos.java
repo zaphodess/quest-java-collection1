6f6cacfab7a48379d95e14e021111894427ce98a
bastienwcs <bastien@wildcodeschool.fr>
2019-03-07 17:10:28 +0100

    init

---
+++ Thanos.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Thanos {
+
+    public static void main(String[] args) {
+
+        // TODO 1 : Create an empty heroes list
+
+        // TODO 2 : Add those heroes to the list
+        // name: Black Widow, age: 34
+        // name: Captain America, age: 100
+        // name: Vision, age: 3
+        // name: Iron Man, age: 48
+        // name: Scarlet Witch, age: 29
+        // name: Thor, age: 1500
+        // name: Spider-Man, age: 18
+        // name: Hulk, age: 49
+        // name: Doctor Strange, age: 42
+
+        // TODO 3 : It's Thor birthday, now he's 1501
+
+        // TODO 4 : Shuffle the heroes list
+
+        // TODO 5 : Keep only the half of the list
+
+        // TODO 6 : Loop throught the list and display the name of the remaining heroes
+    }
+}
